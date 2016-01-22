f2f83d87655a1243e1cd63f63d0468e55822e51f
adalbertofjr <adalberto.fernandesjr@gmail.com>
2016-01-22 21:23:53 -0200

    Alterado o titulo nova viagem

--- app/src/main/java/com/adalbertofjr/minhaviagem/ui/NovaViagemActivity.java
+++ app/src/main/java/com/adalbertofjr/minhaviagem/ui/NovaViagemActivity.java
@@ -17,6 +17,7 @@ public class NovaViagemActivity extends AppCompatActivity implements View.OnClic
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nova_viagem);
+        getSupportActionBar().setTitle("Nova Viagem");
 
     }
 
