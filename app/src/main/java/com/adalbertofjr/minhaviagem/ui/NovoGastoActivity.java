a7ab4d8fd50729545f80171e5621ea8d4a2add17
adalbertofjr <adalberto.fernandesjr@gmail.com>
2016-01-24 10:39:00 -0200

    Adicionado Scrollview

--- app/src/main/java/com/adalbertofjr/minhaviagem/ui/NovoGastoActivity.java
+++ app/src/main/java/com/adalbertofjr/minhaviagem/ui/NovoGastoActivity.java
@@ -14,5 +14,6 @@ public class NovoGastoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_novo_gasto);
+        getSupportActionBar().setTitle("Novo gasto - Recife");
     }
 }
